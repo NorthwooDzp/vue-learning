ff200d01d23fc502a9cec2feb63f3eddccf4bd21
NorthwooDzp <northwood@list.ru>
2019-03-04 01:13:54 +0200

    b4l9

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Car :carName="carName"
+        @nameChanged="changeName($event)"
+        :changeFunc="changeNameToAudi"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Car from './components/Car';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Car
+        },
+        data() {
+            return {
+                carName: 'Ford'
+            }
+        },
+        methods: {
+            changeName(name) {
+                this.carName = name;
+            },
+            changeNameToAudi() {
+                this.carName = 'Audi'
+            }
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Car.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="car">
+        <h4>Name: {{carName}} \ {{reverseName}} </h4>
+        <h4><i>Year: {{year}}</i></h4>
+        <button @click="emitName('not ford')">Change name</button>
+        <button @click="changeFunc()"> Change name 2</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Car',
+        props: {
+            'carName': {
+                type: String,
+                required: false,
+                default: 'some name'
+            },
+            changeFunc: Function
+        },
+        data() {
+            return {
+                year: 2018
+            };
+        },
+        computed: {
+            reverseName() {
+                return this.carName.split('').reverse().join('');
+            }
+        },
+        methods: {
+            emitName(name) {
+                this.$emit('nameChanged', name);
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .car {
+        border: 3px solid yellow;
+        padding: 14px 24px;
+        border-radius: 9px;
+        background-color: aqua;
+    }
+</style>
