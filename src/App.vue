a9dcef1e19024decab1fbf6f6bbcf4b2ac576b3f
NorthwooDzp <northwood@list.ru>
2019-03-03 22:53:09 +0200

    version updated, components module started

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-    <div id="app" class="app">
-        <Car/>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    import Car from './components/Car';
+import HelloWorld from './components/HelloWorld.vue'
 
-    export default {
-        name: 'app',
-        components: {
-            Car
-        }
-    };
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: left;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-    .app {
-        background: lightblue;
-    }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
