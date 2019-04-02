55f67f27541ae09fd87da04d7572b42581e44180
NorthwooDzp <northwood@list.ru>
2019-04-02 06:29:01 +0300

    forms without custom input completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h2>Form inputs</h2>
+        <div>
+            <h4>text input</h4>
+            <input type="text" v-model.lazy="name">
+            <hr>
+            <p>{{name}}</p>
+        </div>
+        <div>
+            <h4>textarea</h4>
+            <textarea v-model.lazy="textarea"></textarea>
+            <hr>
+            <p>{{textarea}}</p>
+        </div>
+        <div>
+            <h4>single checkboxes</h4>
+            <label><input type="checkbox" v-model="checkboxes.vk">VK</label>
+            <label><input type="checkbox" v-model="checkboxes.ok">OK</label>
+            <label><input type="checkbox" v-model="checkboxes.fb">FB</label>
+            <hr>
+            <p>{{JSON.stringify(checkboxes)}}</p>
+        </div>
+        <div>
+            <h4>checkboxes array</h4>
+            <label><input type="checkbox" v-model="social" value="vk">VK</label>
+            <label><input type="checkbox" v-model="social" value="ok">OK</label>
+            <label><input type="checkbox" v-model="social" value="fb">FB</label>
+            <hr>
+            <p>{{JSON.stringify(social)}}</p>
+        </div>
+        <div>
+            <h4>radio buttons</h4>
+            <label><input type="radio" v-model="selectedRadio" value="vk">VK</label>
+            <label><input type="radio" v-model="selectedRadio" value="ok">OK</label>
+            <label><input type="radio" v-model="selectedRadio" value="fb">FB</label>
+            <hr>
+            <p>{{selectedRadio}}</p>
+        </div>
+        <div>
+            <h4>select</h4>
+            <select v-model="selectVal">
+                <option v-for="opt of selectOpts"
+                        :value="opt"
+                >
+                    <!--if attribute 'value is not defined option text willbe used as value'-->
+                    {{opt.toUpperCase()}}
+                </option>
+            </select>
+            <hr>
+            <p>{{selectVal}}</p>
+        </div>
+        <div>
+            <h4>numeric modifier</h4>
+            <input type="text" v-model.number="numberVal">
+            <hr>
+            <p>{{typeof numberVal}} , {{numberVal}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                name: 'test name',
+                textarea: 'test textarea',
+                checkboxes: {
+                    vk: false,
+                    ok: true,
+                    fb: false
+                },
+                social: ['vk'],
+                selectedRadio: 'fb',
+                selectVal: '',
+                selectOpts: ['vk', 'ok', 'fb'],
+                numberVal: 20
+            };
+        }
+    };
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+    }
+
+    #app > div {
+        padding: 12px;
+        border-radius: 5px;
+        border: 1px solid lightblue;
+        margin: 10px auto;
+    }
+
+    textarea {
+        height: 100px;
+        width: 400px;
+    }
+
+    p {
+        white-space: pre;
+    }
+
+    label {
+        margin-right: 20px;
+    }
 </style>
