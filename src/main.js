8ec9e0db79f534b6e25007531831df3c36897de8
NorthwooDzp <northwood@list.ru>
2019-04-02 23:50:50 +0300

    validations b8l1 complete

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+
+Vue.use(Vuelidate);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
