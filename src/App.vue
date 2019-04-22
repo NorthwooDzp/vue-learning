561b86988cfe798bc71b0fa7ed936a3941722376
NorthwooDzp <northwoodzp@gmail.com>
2019-04-22 13:11:26 +0300

    b8l4 done

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
                 <input type="email"
                        id="email"
                        class="form-control"
-                       @change="$v.email.$touch()"
+                       @blur="$v.email.$touch()"
                        v-model="email"
                        :class="{'is-invalid': $v.email.$error}"
                 >
@@ -21,28 +21,79 @@
                     Required field
                 </div>
             </div>
-            <pre>
-                {{$v.email}}
-            </pre>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password"
+                       id="password"
+                       class="form-control"
+                       :class="{'is-invalid': $v.password.$error}"
+                       v-model="password"
+                       @blur="$v.password.$touch()"
+                >
+                <div class="invalid-feedback"
+                     v-if="$v.password.$error && !$v.password.minLength"
+                >
+                    Password should contains at least {{$v.password.$params.minLength.min}} symbols. Now it has {{password.length}} symbols.
+                </div>
+                <div class="invalid-feedback"
+                     v-if="$v.password.$error && !$v.password.required"
+                >
+                    Required field
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="passwordConfirmation">Re-enter Password</label>
+                <input type="password"
+                       id="passwordConfirmation"
+                       class="form-control"
+                       :class="{'is-invalid': $v.passwordConfirmation.$error}"
+                       v-model="passwordConfirmation"
+                       @blur="$v.passwordConfirmation.$touch()"
+                >
+                <div class="invalid-feedback"
+                     v-if="$v.passwordConfirmation.$error && !$v.passwordConfirmation.sameAs"
+                >
+                    Passwords does not match
+                </div>
+                <div class="invalid-feedback"
+                     v-if="$v.passwordConfirmation.$error && !$v.passwordConfirmation.required"
+                >
+                    Required field
+                </div>
+            </div>
         </form>
+        <pre>
+            {{$v}}
+        </pre>
     </div>
 
 </template>
 
 <script>
-    import {required, email} from 'vuelidate/lib/validators'
+    import { required, email, minLength, sameAs } from 'vuelidate/lib/validators';
 
     export default {
         name: 'app',
         data() {
             return {
-                email: ''
-            }
+                email: '',
+                password: '',
+                passwordConfirmation: ''
+            };
         },
         validations: {
             email: {
                 required,
                 email
+            },
+            password: {
+                required,
+                minLength: minLength(6)
+            },
+            passwordConfirmation: {
+                required,
+                // sameAs: sameAs('password')
+                sameAs: sameAs((vue) => vue.password) // correct way
             }
         }
     };
@@ -50,8 +101,8 @@
 
 <style lang="scss">
     .form-group {
-            padding: 16px;
-            border-radius: 5px;
-            border: 1px solid lightblue;
+        padding: 16px;
+        border-radius: 5px;
+        border: 1px solid lightblue;
     }
 </style>
