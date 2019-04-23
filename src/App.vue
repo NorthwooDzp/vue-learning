e8517201b641760344ce332b94153c66988ee9b7
NorthwooDzp <northwoodzp@gmail.com>
2019-04-23 11:05:02 +0300

    b9l4 active links done

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,52 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link>
-            |
-            <router-link to="/about">About</router-link>
+    <div class="container">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <router-link to="/"
+                             tag="a"
+                             class="nav-link"
+                             exact active-class="active"
+                >
+                    Home
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link :to="'/cars'"
+                             tag="a"
+                             class="nav-link"
+                             active-class="active"
+                >
+                    Cars
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/about"
+                             tag="a"
+                             class="nav-link"
+                             active-class="active"
+                >
+                    About
+                </router-link>
+            </li>
+        </ul>
+        <div class="content">
+            <router-view/>
         </div>
-        <router-view/>
     </div>
 </template>
 
+<script>
+    export default {
+        beforeCreate() {
+            // console.log(process.env);
+        }
+    };
+</script>
+
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
+
+    .content {
         text-align: center;
-        color: #2c3e50;
     }
 
-    #nav {
-        padding: 30px;
-    }
-
-    #nav a {
-        font-weight: bold;
-        color: #2c3e50;
-    }
-
-    #nav a.router-link-exact-active {
-        color: #42b983;
-    }
 </style>
---
+++ src/views/Cars.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Cars page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Cars'
+    };
+</script>
+
+<style scoped>
+
+</style>
